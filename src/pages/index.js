36008ef53d207c272e2f55aab85f46bc33900538
mirunaen <mirunaen@gmail.com>
2020-05-27 19:07:59 +0200

    minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ const IndexPage = () => {
   return (
     <div>
       <h1>Hello!</h1>
-      <h2>I`m Miruna,a teenager trying Gatsby</h2>
+      <h2>I`m Miruna,a teenager living in beautiful Spain</h2>
       <p>Need a developer?</p> <a href="/contact">Contact me!</a>
       <p>
         Need a developer? <Link to="/contact">Contact me </Link>{" "}
